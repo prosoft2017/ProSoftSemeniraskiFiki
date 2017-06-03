6bab8fa69fe9bfdb257e0df58a2400ed2158f82f
Filip <djordjevic.filip994@gmail.com>
2017-06-03 17:48:32 +0200

    Filter

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,9 @@
  */
 package main;
 
+import javax.swing.JDialog;
+import view.login.JDialogLoginWindow;
+
 /**
  *
  * @author Nikola
@@ -15,31 +18,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(view.main.JFrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            javax.swing.JFrame jfr = new view.main.JFrameMain();
-//            jfr.setLocationRelativeTo(null);
-            jfr.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-            jfr.setVisible(true);
-        });
+        JDialog loginDialog = new JDialogLoginWindow(null, true);
+        loginDialog.setLocationRelativeTo(null);
+        loginDialog.setVisible(true);
     }
 }
